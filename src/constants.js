87a7c7d496b11918d573523510db7e2f048f9928
IoPhoenix <olga.fomin26@gmail.com>
2018-09-14 15:40:34 -0700

    Changed development server link to production

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
-export const DATABASE_LINK = 'http://localhost:3000';
+export const DATABASE_LINK = 'https://calm-forest-65718.herokuapp.com';
 
+// development: http://localhost:3000
 // production: https://calm-forest-65718.herokuapp.com
