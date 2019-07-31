1150f4137e2c16212b31430a9ee0a863234594f1
Olga Fomin <olga.fomin26@gmail.com>
2019-07-31 08:41:05 -0700

    Add lambda function for Rank badge

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,14 +2,44 @@ import React from 'react';
 import { capitalize } from '../../helpers';
 
 
-const Rank = (props) => {
-  return (
-    <div>
-      <div className='white f3 mb5'>
-        {`${capitalize(props.name)}, your # of entries is ${props.entries}`} 
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: 'test'
+    }
+  }
+  
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+  
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries === this.props.entries) {
+      return null;
+    }
+    
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://v56u3wcym0.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+    .then(response => response.json())
+    .then(data => console.log('data: ', data))
+    .then(data => this.setState({ emoji: data.input}))
+    .catch(console.log);
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className='white f3 mb5'>
+          {`${capitalize(this.props.name)}, your # of entries is ${this.props.entries} ${this.state.emoji}`} 
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Rank;
